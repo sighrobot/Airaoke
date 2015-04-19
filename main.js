0f1c372349c7efec576d98da0305f32e70318a8f
Seth Kranzler <sethkranzler@gmail.com>
2015-04-19 15:59:22 -0400

    Added instruments

--- main.js
+++ main.js
@@ -1,25 +1,105 @@
-var total = 0;
-var differencePast = 0;
-var differenceAvg = 0;
+var bongo = new Tone.Sampler({
+    "bongo_hit": "./samples/bongo/bongo_hit.wav",
+    "bongo_slap": "./samples/bongo/bongo_slap.wav"
+}).toMaster();
+
+var snare = new Tone.Sampler({
+    "snare": "./samples/snare/snare1.wav"
+}).toMaster();
+
+var guitar = new Tone.Sampler({
+    "guitar1": "./samples/guitar/1.wav",
+    "guitar2": "./samples/guitar/2.wav",
+    "guitar3": "./samples/guitar/3.wav",
+    "guitar4": "./samples/guitar/4.wav",
+    "guitar5": "./samples/guitar/5.wav",
+    "guitar6": "./samples/guitar/6.wav",
+    "guitar7": "./samples/guitar/7.wav",
+    "guitar8": "./samples/guitar/8.wav"
+}).toMaster();
+
+
 var lastMove = 0;
 
-doppler.init(function(bandwidth) { 
-    difference = bandwidth.left - bandwidth.right - 1;
-    differenceAvg += difference;
-    differenceAvg = differenceAvg / 2;
-    // console.log(differenceAvg);
-    
-    //cumulative function adds movement up so slower movements can still be registered
-    if (difference !== differencePast) {
-        total += difference;
-        console.log(total);
+doppler.init(function(bandwidth) {
+    difference = bandwidth.right - bandwidth.left - 1;
+    console.log(difference);
+    if (toggle1.val.value) {
+        playBongo(difference);
+    }
+    if (toggle2.val.value) {
+        playSnare(difference);
+    }
+    if (toggle3.val.value) {
+        playGuitar(difference);
+    }
+});
+
+var lastVal = 0;
+var bongoCount = 0;
+
+playBongo = function(diff) {
+    if (diff > 15 && Date.now() - lastMove > 75) {
+
+        if (bongoCount < 3) {
+            bongo.triggerAttack("bongo_hit");
+            bongoCount++;
+        } else {
+            bongo.triggerAttack("bongo_slap");
+            bongoCount = 0;
+        }
         lastMove = Date.now();
     }
-    //resets the additive function if there is no movement past 1 second 
-    if (difference == differencePast && Date.now() - lastMove > 1000) {
-        total = 0;
-        console.log("reset");
-    }
-    differencePast = difference;
+    lastVal = diff;
+}
 
-});
+playSnare = function(diff) {
+    if (diff > 15 && Date.now() - lastMove > 50) {
+
+        snare.triggerAttack("snare");
+        lastMove = Date.now();
+    }
+    lastVal = diff;
+}
+
+var guitarCount = 1;
+playGuitar = function(diff) {
+    if (diff > 15 && Date.now() - lastMove > 150) {
+
+        if (guitarCount > 8) {
+            guitarCount = 1;
+        }
+        var guitarNumber = "guitar" + guitarCount.toString();
+        guitar.triggerAttack(guitarNumber);
+        guitarCount++;
+
+        lastMove = Date.now();
+        lastVal = diff;
+    }
+
+
+}
+
+//saving for later
+// var total = 0;
+// var differencePast = 0;
+// var differenceAvg = 0;
+// var lastMove = 0;
+
+
+// differenceAvg += difference;
+// differenceAvg = differenceAvg / 2;
+// console.log(difference);
+
+// //cumulative function adds movement up so slower movements can still be registered
+// if (difference !== differencePast) {
+//     total += difference;
+//     //console.log(total);
+//     lastMove = Date.now();
+// }
+// //resets the additive function if there is no movement past 1 second 
+// if (difference == differencePast && Date.now() - lastMove > 1000) {
+//     total = 0;
+//     //console.log("reset");
+// }
+// differencePast = difference;
---
+++ todo.md
@@ -0,0 +1,13 @@
+# to improve:
+peak detection
+set amplitude (threshold is passed, then amplitude of signal determines volume)
+further expression?
+Mapping to filter/other effects
+
+# to add:
+select instrument
+record loop
+violin
+piano
+tambourine
+other instruments where the gesture is obvious
